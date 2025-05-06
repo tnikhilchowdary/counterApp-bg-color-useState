ca6495ed34cfa07bd61d93b22fd5b02c37a5490b
tnikhilchowdary <tnikilchowdary@gmail.com>
2025-05-05 23:16:03 -0500

    Initial commit - Counter App with background color change using useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {react, useState} from 'react';
+
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [bgColor, setBgColor] = useState("white");
+
+  function Inc(){
+    setCount(prev => prev + 1);
+    setBgColor("lightgreen");
+  }
+
+  function Dec(){
+    setCount(prev => prev - 1);
+    setBgColor("lightcoral");
+
+  }
+
+  function Reset(){
+    setCount(0);
+    setBgColor("lightblue");
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor:bgColor}}>
+      <h1>Counter App</h1>
+      <div className='button-group'>
+      <button onClick={Inc}>Increment</button>
+      <button onClick={Reset}>Reset</button>
+      <button onClick={Dec}>Decrement</button>
+      </div>
+      <h1>{count}</h1>
     </div>
   );
 }
